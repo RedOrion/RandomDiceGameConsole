2bdfb5f76526d3150e18341e00a0a8a8bd8b56d5
Bernard <redorion0@icloud.com>
2014-09-12 11:48:10 -0400

    Add option for second die

--- RandomDiceGameConsole/Module1.vb
+++ RandomDiceGameConsole/Module1.vb
@@ -1,29 +1,65 @@
 ﻿Module Module1
 
     Sub Main()
-        Console.WriteLine("Please Select a Value between 1 and 6")
-        Dim chosenValue = Console.ReadLine()
-        Console.WriteLine("Chosen Value")
-        Console.WriteLine(chosenValue)
+    
+    	Console.WriteLine("Choose 1 or 2 Dice")
+    	Dim chosenDice = Console.ReadLine()
+    	
+    	If chosenDie = 1 Then
+    
+        	Console.WriteLine("Please Select a Value between 1 and 6")
+        	Dim chosenValue = Console.ReadLine()
+        	Console.WriteLine("Chosen Value")
+        	Console.WriteLine(chosenValue)
 
-        Console.ReadKey()
-        Randomize()
-        Dim sixsideddie As Integer = CInt(Int((6 * Rnd()) + 1))
-        Console.WriteLine("Die Role")
-        Console.WriteLine(sixsideddie)
+		 	Console.ReadKey()
+       		Randomize()
+        	Dim sixsideddie As Integer = CInt(Int((6 * Rnd()) + 1))
+        	Console.WriteLine("Die Role")
+        	Console.WriteLine(sixsideddie)
 
-        Console.WriteLine("Press Enter when you are ready to see results")
-        Console.ReadKey()
+        	Console.WriteLine("Press Enter when you are ready to see results")
+        	Console.ReadKey()
 
-        If chosenValue = sixsideddie Then
-            Console.WriteLine("You Won")
-            'Console.WriteLine(chosenValue "=" sixsideddie)
-            Console.ReadKey()
+        	If chosenValue = sixsideddie Then
+            	Console.WriteLine("You Won")
+            	'Console.WriteLine(chosenValue "=" sixsideddie)
+            	Console.ReadKey()
+        	Else
+            	Console.WriteLine("You Lost")
+            	'Console.WriteLine(chosenValue "≠" sixsideddie)
+            	Console.ReadKey()
+        	End If
+        
+        ElseIf chosenDie = 2 Then
+        	
+        	Console.WriteLine("Please Select a Value between 1 and 12")
+        	Dim chosenValue = Console.ReadLine()
+        	Console.WriteLine("Chosen Value")
+        	Console.WriteLine(chosenValue)
+
+		 	Console.ReadKey()
+       		Randomize()
+        	Dim twosixsideddie As Integer = CInt(Int((12 * Rnd()) + 1))
+        	Console.WriteLine("Die Role")
+        	Console.WriteLine(sixsideddie)
+
+        	Console.WriteLine("Press Enter when you are ready to see results")
+        	Console.ReadKey()
+
+        	If chosenValue = twosixsideddie Then
+            	Console.WriteLine("You Won")
+            	'Console.WriteLine(chosenValue "=" twosixsideddie)
+            	Console.ReadKey()
+        	Else
+            	Console.WriteLine("You Lost")
+            	'Console.WriteLine(chosenValue "≠" twosixsideddie)
+            	Console.ReadKey()
+        	End If
+        
         Else
-            Console.WriteLine("You Lost")
-            'Console.WriteLine(chosenValue "≠" sixsideddie)
-            Console.ReadKey()
-        End If
+        	Die
+        
     End Sub
 
 End Module
