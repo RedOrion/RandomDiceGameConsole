cb2c7af420d16c2380f8898c7b672f85646ac669
RedOrion <redorion0@gmail.com>
2014-09-17 09:44:56 -0400

    Update Module1.vb

--- RandomDiceGameConsole/Module1.vb
+++ RandomDiceGameConsole/Module1.vb
@@ -1,69 +1,71 @@
- Module Module1
+Module Module1
 
     Sub Main()
-    	Do
-    	Console.WriteLine("Choose 1 or 2 Dice")
-    	Dim chosenDice = Console.ReadLine()
-    	
-    	If chosenDice = 1 Then
-    
-		Console.WriteLine("Please Select a Value between 1 and 6")
-		Dim chosenValue = Console.ReadLine()
-		Console.WriteLine("Chosen Value")
-		Console.WriteLine(chosenValue)
+        Do
+            Console.WriteLine("Choose 1 or 2 Dice")
+            Dim chosenDice = Console.ReadLine()
 
-		Console.ReadKey()
-		Randomize()
-		Dim totaldierole As Integer = CInt(Int((6 * Rnd()) + 1))
-		Console.WriteLine("Die Role")
-		Console.WriteLine(totaldierole)
+            If chosenDice = 1 Then
 
-		Console.WriteLine("Press Enter when you are ready to see results")
-		Console.ReadKey()
+                Console.WriteLine("Please Select a Value between 1 and 6")
+                Dim chosenValue = Console.ReadLine()
+                Console.WriteLine("Chosen Value")
+                Console.WriteLine(chosenValue)
 
-		If chosenValue = totaldierole Then
-            		Console.WriteLine("You Won")
-            		'Console.WriteLine(chosenValue "=" totaldierole)
-            		Console.ReadKey()
-		Else
-            		Console.WriteLine("You Lost")
-            		'Console.WriteLine(chosenValue "≠" totaldierole)
-            		Console.ReadKey()
-		End If
-        
-        ElseIf chosenDice = 2 Then
-        	
-		Console.WriteLine("Please Select a Value between 1 and 12")
-		Dim chosenValue = Console.ReadLine()
-		Console.WriteLine("Chosen Value")
-		Console.WriteLine(chosenValue)
+                Console.ReadKey()
+                Randomize()
+                Dim totaldierole As Integer = CInt(Int((6 * Rnd()) + 1))
+                Console.WriteLine("Die Role")
+                Console.WriteLine(totaldierole)
 
-		Console.ReadKey()
-            	Randomize()
-		Dim onesixsideddie As Integer = CInt(Int((6 * Rnd()) + 1))
-            	Dim twosixsideddie As Integer = CInt(Int((6 * Rnd()) + 1))
-            	Dim totaldierole As Integer
-            	totaldierole = onesixsideddie + twosixsideddie
-            	Console.WriteLine("Die Role")
-            	Console.WriteLine(totaldierole)
+                Console.WriteLine("Press Enter when you are ready to see results")
+                Console.ReadKey()
 
-		Console.WriteLine("Press Enter when you are ready to see results")
-		Console.ReadKey()
+                If chosenValue = totaldierole Then
+                    Console.WriteLine("You Won")
+                    'Console.WriteLine(chosenValue "=" totaldierole)
+                    Console.ReadKey()
+                Else
+                    Console.WriteLine("You Lost")
+                    'Console.WriteLine(chosenValue "≠" totaldierole)
+                    Console.ReadKey()
+                End If
 
-		If chosenValue = totaldierole Then
-            		Console.WriteLine("You Won")
-            		'Console.WriteLine(chosenValue "=" totaldierole)
-            		Console.ReadKey()
-		Else
-            		Console.WriteLine("You Lost")
-            		'Console.WriteLine(chosenValue "≠" totaldierole)
-            		Console.ReadKey()
-		End If
-        
-	End If
-	Console.WriteLine("Restarting Game")
-	Loop
-        
+            ElseIf chosenDice = 2 Then
+
+                Console.WriteLine("Please Select a Value between 1 and 12")
+                Dim chosenValue = Console.ReadLine()
+                Console.WriteLine("Chosen Value")
+                Console.WriteLine(chosenValue)
+
+                Console.ReadKey()
+                Randomize()
+                Dim onesixsideddie As Integer = CInt(Int((6 * Rnd()) + 1))
+                Dim twosixsideddie As Integer = CInt(Int((6 * Rnd()) + 1))
+                Dim totaldierole As Integer
+                totaldierole = onesixsideddie + twosixsideddie
+                Console.WriteLine("Die Role")
+                Console.WriteLine(totaldierole)
+
+                Console.WriteLine("Press Enter when you are ready to see results")
+                Console.ReadKey()
+
+                If chosenValue = totaldierole Then
+                    Console.WriteLine("You Won")
+                    'Console.WriteLine(chosenValue "=" totaldierole)
+                    Console.ReadKey()
+                Else
+                    Console.WriteLine("You Lost")
+                    'Console.WriteLine(chosenValue "≠" totaldierole)
+                    Console.ReadKey()
+                End If
+
+            End If
+            Console.WriteLine("Restarting Game")
+            Console.WriteLine()
+        Loop
+
     End Sub
+
 
 End Module
