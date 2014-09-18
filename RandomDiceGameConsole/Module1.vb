2aeb6d9dcd68b5a6df6dba6ddfa944c0f30845ab
RedOrion <redorion0@gmail.com>
2014-09-18 10:17:45 -0400

    Adding Error Checking

--- RandomDiceGameConsole/Module1.vb
+++ RandomDiceGameConsole/Module1.vb
@@ -12,30 +12,30 @@ Module Module1
                 Console.WriteLine("Chosen Value")
                 Console.WriteLine(chosenValue)
 
+                Console.WriteLine("Press Enter to see the die role.")
                 Console.ReadKey()
                 Randomize()
                 Dim totaldierole As Integer = CInt(Int((6 * Rnd()) + 1))
                 Console.WriteLine("Die Role")
                 Console.WriteLine(totaldierole)
-
-                Console.WriteLine("Press Enter when you are ready to see results")
                 Console.ReadKey()
 
                 If chosenValue = totaldierole Then
-                    Console.WriteLine("You Won")
+                    Console.WriteLine("You Won!")
                     Console.ReadKey()
-                Else
-                    Console.WriteLine("You Lost")
+                ElseIf chosenValue <> totaldierole Then
+                    Console.WriteLine("You Lost!")
                     Console.ReadKey()
                 End If
 
             ElseIf chosenDice = 2 Then
 
-                Console.WriteLine("Please Select a Value between 1 and 12")
+                Console.WriteLine("Please Select a Value between 1 and 12.")
                 Dim chosenValue = Console.ReadLine()
                 Console.WriteLine("Chosen Value")
                 Console.WriteLine(chosenValue)
 
+                Console.WriteLine("Press Enter to see the die roles.")
                 Console.ReadKey()
                 Randomize()
                 Dim onesixsideddie As Integer = CInt(Int((6 * Rnd()) + 1))
@@ -44,24 +44,35 @@ Module Module1
                 totaldierole = onesixsideddie + twosixsideddie
                 Console.WriteLine("Die Role")
                 Console.WriteLine(totaldierole)
-
-                Console.WriteLine("Press Enter when you are ready to see results")
                 Console.ReadKey()
 
                 If chosenValue = totaldierole Then
                     Console.WriteLine("You Won")
                     Console.ReadKey()
-                Else
+                ElseIf chosenValue <> totaldierole Then
                     Console.WriteLine("You Lost")
                     Console.ReadKey()
                 End If
             ElseIf chosenDice <> 1 <> 2 Then
-                Console.WriteLine("Value was incorrect")
-                Console.WriteLine("You need to choose either 1 or 2")
-            Else
-
+                Console.WriteLine("The value was incorrect.")
+                Console.WriteLine("You need to choose either 1 or 2.")
             End If
+
+            'Console.WriteLine("Do you want to start another game?")
+            'Console.WriteLine("Please select Y or N.")
+            'Dim ContinueYesOrNo = Console.ReadLine()
+            'Dim Y As Single
+            'Dim N As Single
+            'If ContinueYesOrNo = Y Then
+            'Console.WriteLine("New Game Starting Now.")
+            'ElseIf ContinueYesOrNo = N Then
+            'System.Environment.Exit(1)
+            'ElseIf ContinueYesOrNo <> Y <> N Then
+            'Console.WriteLine("You need to choose either Y or N.")
+            'End If
+
             Console.WriteLine("Restarting Game")
+            Console.WriteLine()
             Console.WriteLine()
         Loop
 
