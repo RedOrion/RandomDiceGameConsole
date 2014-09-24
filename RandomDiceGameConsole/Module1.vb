da8101ac76e0dc5d84a88cb3bcbaab50646946e6
RedOrion <redorion0@icloud.com>
2014-09-24 09:41:08 -0400

    Add: Restart or Exit Game

--- RandomDiceGameConsole/Module1.vb
+++ RandomDiceGameConsole/Module1.vb
@@ -2,81 +2,93 @@ Module Module1
 
     Sub Main()
         Do
-            Console.WriteLine("Choose 1 or 2 Dice")
-            Dim chosenDice = Console.ReadLine()
+            Do
+                Console.WriteLine("Choose 1 or 2 Dice")
+                Dim chosenDice = Console.ReadLine()
 
-            If chosenDice = 1 Then
-
-                Console.WriteLine("Please Select a Value between 1 and 6")
-                Dim chosenValue = Console.ReadLine()
-                Console.WriteLine("Chosen Value")
-                Console.WriteLine(chosenValue)
-
-                Console.WriteLine("Press Enter to see the die role.")
-                Console.ReadKey()
-                Randomize()
-                Dim totaldierole As Integer = CInt(Int((6 * Rnd()) + 1))
-                Console.WriteLine("Die Role")
-                Console.WriteLine(totaldierole)
-                Console.ReadKey()
-
-                If chosenValue = totaldierole Then
-                    Console.WriteLine("You Won!")
-                    Console.ReadKey()
-                ElseIf chosenValue <> totaldierole Then
-                    Console.WriteLine("You Lost!")
-                    Console.ReadKey()
+                If chosenDice = 1 Then
+                    OneDie()
+                    If chosenDice = 1 Then
+                        Exit Do
+                    End If
+                ElseIf chosenDice = 2 Then
+                    TwoDice()
+                    If chosenDice = 2 Then
+                        Exit Do
+                    End If
+                ElseIf chosenDice <> 1 <> 2 Then
+                    Console.WriteLine("The value was incorrect.")
+                    Console.WriteLine("You need to choose either 1 or 2.")
                 End If
-
-            ElseIf chosenDice = 2 Then
-
-                Console.WriteLine("Please Select a Value between 1 and 12.")
-                Dim chosenValue = Console.ReadLine()
-                Console.WriteLine("Chosen Value")
-                Console.WriteLine(chosenValue)
-
-                Console.WriteLine("Press Enter to see the die roles.")
-                Console.ReadKey()
-                Randomize()
-                Dim onesixsideddie As Integer = CInt(Int((6 * Rnd()) + 1))
-                Dim twosixsideddie As Integer = CInt(Int((6 * Rnd()) + 1))
-                Dim totaldierole As Integer
-                totaldierole = onesixsideddie + twosixsideddie
-                Console.WriteLine("Die Role")
-                Console.WriteLine(totaldierole)
-                Console.ReadKey()
-
-                If chosenValue = totaldierole Then
-                    Console.WriteLine("You Won")
-                    Console.ReadKey()
-                ElseIf chosenValue <> totaldierole Then
-                    Console.WriteLine("You Lost")
-                    Console.ReadKey()
-                End If
-            ElseIf chosenDice <> 1 <> 2 Then
-                Console.WriteLine("The value was incorrect.")
-                Console.WriteLine("You need to choose either 1 or 2.")
-            End If
-
-            'Console.WriteLine("Do you want to start another game?")
-            'Console.WriteLine("Please select Y or N.")
-            'Dim ContinueYesOrNo = Console.ReadLine()
-            'Dim Y As Single
-            'Dim N As Single
-            'If ContinueYesOrNo = Y Then
-            'Console.WriteLine("New Game Starting Now.")
-            'ElseIf ContinueYesOrNo = N Then
-            'System.Environment.Exit(1)
-            'ElseIf ContinueYesOrNo <> Y <> N Then
-            'Console.WriteLine("You need to choose either Y or N.")
-            'End If
-
-            Console.WriteLine("Restarting Game")
-            Console.WriteLine()
-            Console.WriteLine()
+            Loop
+            RestartYesOrNo()
         Loop
 
     End Sub
 
+    Sub OneDie()
+        Console.WriteLine("Please Select a Value between 1 and 6")
+        Dim chosenValue = Console.ReadLine()
+        Console.WriteLine("Chosen Value")
+        Console.WriteLine(chosenValue)
+
+        Console.WriteLine("Press Enter to see the die role.")
+        Console.ReadKey()
+        Randomize()
+        Dim totaldierole As Integer = CInt(Int((6 * Rnd()) + 1))
+        Console.WriteLine("Die Role")
+        Console.WriteLine(totaldierole)
+        Console.ReadKey()
+
+        If chosenValue = totaldierole Then
+            Console.WriteLine("You Won!")
+            Console.ReadKey()
+        ElseIf chosenValue <> totaldierole Then
+            Console.WriteLine("You Lost!")
+            Console.ReadKey()
+        End If
+    End Sub
+
+    Sub TwoDice()
+        Console.WriteLine("Please Select a Value between 1 and 12.")
+        Dim chosenValue = Console.ReadLine()
+        Console.WriteLine("Chosen Value")
+        Console.WriteLine(chosenValue)
+
+        Console.WriteLine("Press Enter to see the die roles.")
+        Console.ReadKey()
+        Randomize()
+        Dim onesixsideddie As Integer = CInt(Int((6 * Rnd()) + 1))
+        Dim twosixsideddie As Integer = CInt(Int((6 * Rnd()) + 1))
+        Dim totaldierole As Integer
+        totaldierole = onesixsideddie + twosixsideddie
+        Console.WriteLine("Die Role")
+        Console.WriteLine(totaldierole)
+        Console.ReadKey()
+
+        If chosenValue = totaldierole Then
+            Console.WriteLine("You Won")
+            Console.ReadKey()
+        ElseIf chosenValue <> totaldierole Then
+            Console.WriteLine("You Lost")
+            Console.ReadKey()
+        End If
+    End Sub
+
+
+
+    Sub RestartYesOrNo()
+        Console.WriteLine("Do you want to start another game?")
+        Console.WriteLine("Please type 1 to restart the game or 0 to end the game.")
+        Dim ContinueYesOrNo = Console.ReadLine()
+        If ContinueYesOrNo = 0 Then
+            System.Environment.Exit(1)
+        ElseIf ContinueYesOrNo <> 1 <> 0 Then
+            Console.WriteLine("Please type 1 to restart the game or 0 to end the game.")
+        Else : ContinueYesOrNo = 1
+            Console.WriteLine("New Game Starting Now.")
+        End If
+
+    End Sub
 
 End Module
